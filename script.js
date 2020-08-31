869335c040a54baa7db2bb5e4415cd4ffaaf112e
unknown <proazadhossain@gmail.com>
2020-09-01 05:23:51 +0600

    Just Git add

---
+++ script.js
@@ -0,0 +1,7 @@
+
+var myfirstVariable = 20,
+    anotherVariable = "Another Variable",
+    lastVariable;
+    lastVariable = myfirstVariable ;
+console.log(lastVariable);
+console.log(anotherVariable);

d2a3f44b0300f8c42b58ad5adb9c9e6b432d803d
unknown <proazadhossain@gmail.com>
2020-09-02 04:08:05 +0600

    Create a calculator by JavaScript

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-var a = 20;
-var b = 10;
-var sum = a+b;
-var sub = a-b;
-var mul = a*b;
-var div = a/b;
+var num1 = prompt("Enter First Number : ");
+var operator = prompt("Enter Operator :");
+var num2 = prompt("Enter Second Number :");
 
-document.write("Sum is = "+sum+" Sub is = "+sub+" mul is = "+mul+" div is = "+div);
+num1 = parseInt(num1, 10);
+num2 = parseInt(num2, 10)
+
+var sum, sub, mul, div;
+if(operator =="+"){
+    sum = num1 + num2;
+    document.write(num1+" + "+num2+" = "+sum+"<br/>");
+} else if(operator =="-"){
+    sub = num1 + num2;
+    document.write(num1+" - "+num2+" = "+sub+"<br/>");
+} else if(operator == '*'){
+    mul = num1 * num2;
+    document.write(num1+" x "+num2+" = "+mul+"<br/>");
+} else if(operator == '/'){
+    div = num1 / num2;
+    document.write(num1+" / "+num2+" = "+div+"<br/>");
+}else{
+    document.write("You Enter a Invalid Number or Operator, try Again");
+}
